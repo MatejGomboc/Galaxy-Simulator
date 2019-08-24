636b6ca053268fe7678b7d16a96ad4d6093046dd
Matej Gomboc <matejg03@gmail.com>
2019-08-24 17:45:24 +0200

    Updated Python script for generating source string.

--- galaxy-simulator/oclProgramFileToString.py
+++ galaxy-simulator/oclProgramFileToString.py
@@ -1,94 +1,78 @@
 #!/usr/bin/python
 
-import sys, argparse, re
+
+import os, sys, argparse, re
+
 
 def comment_remover(text):
-    def replacer(match):
-        s = match.group(0)
-        if s.startswith('/'):
-            return " " # note: a space and not an empty string
-        else:
-            return s
-    pattern = re.compile(
-        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
-        re.DOTALL | re.MULTILINE
-    )
-    return re.sub(pattern, replacer, text)
+	def replacer(match):
+		s = match.group(0)
+		if s.startswith('/'):
+			return " " # note: a space and not an empty string
+		else:
+			return s
 
+	pattern = re.compile(
+		r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+		re.DOTALL | re.MULTILINE
+	)
 
-def empty_string_remover(text):
-	new_text = text
-	new_text_prev = ""
-	while (new_text != new_text_prev):
-		new_text_prev = new_text
-		new_text = new_text.replace(" \"", "\"")
-		new_text = new_text.replace("\t\"", "\"")
-	return new_text
-
-
-def empty_line_remover(text):
-	new_text = text
-	new_text_prev = ""
-	while (new_text != new_text_prev):
-		new_text_prev = new_text
-		new_text = new_text.replace("\n\n", "\n") # remove empty lines
-	return new_text
+	return re.sub(pattern, replacer, text)
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 	parser.add_argument("-c", "--compact", help="remove all comments and empty lines", action="store_true")
-	parser.add_argument("inputFileName", help="name of input OpenCL file with source", type=str)
-	parser.add_argument("outputFileName", help="name of output C++ file with source as char string", type=str)
+	parser.add_argument("inputFileName", help="name of input OpenCL file with source (*.cl, *.clh)", type=str)
+	parser.add_argument("outputFileName", help="name of output C/C++ file with source as C char string (*.c, *.cpp, *.h)", type=str)
 	args = parser.parse_args()
 
-	low_input_name = args.inputFileName.lower()
-	if not low_input_name.endswith(".cl") and not low_input_name.endswith(".clh"):
+	if not args.inputFileName.endswith(".cl") and not args.inputFileName.endswith(".clh"):
 		sys.exit("invalid input file extension: " + args.inputFileName)
-		
-	low_output_name = args.outputFileName.lower()
-	if not low_output_name.endswith(".cpp") and not low_output_name.endswith(".h"):
+
+	if not args.outputFileName.endswith(".c") and not args.outputFileName.endswith(".cpp") and not args.outputFileName.endswith(".h"):
 		sys.exit("invalid output file extension: " + args.outputFileName)
 
 	if args.verbose:
 		print("input file: " + args.inputFileName)
 		print("output file: " + args.outputFileName)
 
-	stringSource = open(args.inputFileName, 'r').read()
+	with open(args.inputFileName, 'r') as file:
+		stringSource = "\n".join([line.rstrip() for line in file]) # remove trailing whitespaces
 
 	if args.verbose:
-		print("\nOpenCL source:")
+		print("\noriginal OpenCL source:")
 		print(stringSource)
 
 	if args.compact:
 		stringSource = comment_remover(stringSource) # remove all comments
+		stringSource = stringSource.replace("\n", "") # remove line breaks
+		stringSource = stringSource.replace("\t", " ") # replace '\t' with single space
+		stringSource = stringSource.replace("   ", " ") # replace triple space with single space
+		stringSource = stringSource.replace("  ", " ") # replace double space with single space
+		stringSource = stringSource.replace("; ", ";") # remove space at the end of each statement
+		stringSource = stringSource.strip(); # remove leading and trailing space
+		
+		# remove spaces at each operator and bracket
+		for char in "!\"%&/()=?*+-|,.;:^{}~\'[]<>":
+			stringSource = stringSource.replace(char + " ", char)
+			stringSource = stringSource.replace(" " + char, char)
 
-	stringSource = stringSource.replace("\\", "\\\\") # for multi-line strings
-	stringSource = stringSource.replace("\"", "\\\"") # for strings
-	stringSource = stringSource.replace("\'", "\\\'") # for chars
-	stringSource = stringSource.replace("\\\a", "\\\\\\\a") # if inside a string
-	stringSource = stringSource.replace("\\\b", "\\\\\\\b") # if inside a string
-	stringSource = stringSource.replace("\\\f", "\\\\\\\f") # if inside a string
-	stringSource = stringSource.replace("\\\r", "\\\\\\\r") # if inside a string
-	stringSource = stringSource.replace("\\\t", "\\\\\\\t") # if inside a string
-	stringSource = stringSource.replace("\\\v", "\\\\\\\v") # if inside a string
-	stringSource = stringSource.replace("\\\n", "\\\\\\\n") # if inside a string
-	stringSource = stringSource.replace("\n", "\\n\"\n\"") # for correct ending of new multi-line string
+	stringSource = stringSource.replace("\\", "\\\\") # replace '\\'
+	stringSource = stringSource.replace("\"", "\\\"") # replace '\"'
+	stringSource = stringSource.replace("\'", "\\\'") # replace '\''
+	stringSource = stringSource.replace("\n", "\\n\"\n\"") # replace '\n'
 
-	stringSource = "const char* ocl_src_" + args.inputFileName[:-3] + " = \n\"" + stringSource + "\"\n"
-
-	stringSource = empty_string_remover(stringSource) # remove redundant spaces and tabs
-	stringSource = stringSource.replace("\"\"", "") # remove empty multi-line string
-	stringSource = stringSource[:-1]
+	stringSource = "const char* ocl_src_" + os.path.splitext(args.inputFileName)[0] + " =\n\"" + stringSource
 
 	if args.compact:
-		stringSource = empty_line_remover(stringSource) # remove empty lines
-		
-	stringSource += "\"\\0\";\n"
+		stringSource += "\";\n"
+	else:
+		stringSource += "\\n\";\n"
 
 	if args.verbose:
-		print("source as C++ char string:")
+		print("\nOpenCL source as C char string:")
 		print(stringSource)
 
 	open(args.outputFileName, "w").write(stringSource)
